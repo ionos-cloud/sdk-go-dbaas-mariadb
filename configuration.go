9b24ae9f97eb78220daa4d7b2357cff4f2022638
Ionos Cloud SDK Robot <sdk@cloud.ionos.com>
2024-10-10 10:07:57 +0000

    auto-generated version 1.1.0

--- configuration.go
+++ configuration.go
@@ -134,7 +134,7 @@ func NewConfiguration(username, password, token, hostUrl string) *Configuration 
 	cfg := &Configuration{
 		DefaultHeader:      make(map[string]string),
 		DefaultQueryParams: url.Values{},
-		UserAgent:          "ionos-cloud-sdk-go-dbaas-mariadb/v1.0.1",
+		UserAgent:          "ionos-cloud-sdk-go-dbaas-mariadb/v1.1.0",
 		Debug:              false,
 		Username:           username,
 		Password:           password,
