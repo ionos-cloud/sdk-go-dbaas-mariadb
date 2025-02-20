cc09245036d951f586e76261727c0f63a2ada2fa
Ionos Cloud SDK Robot <sdk@cloud.ionos.com>
2025-02-20 15:17:00 +0000

    auto-generated version 1.1.3

--- configuration.go
+++ configuration.go
@@ -141,7 +141,7 @@ func NewConfiguration(username, password, token, hostUrl string) *Configuration 
 	cfg := &Configuration{
 		DefaultHeader:      make(map[string]string),
 		DefaultQueryParams: url.Values{},
-		UserAgent:          "ionos-cloud-sdk-go-dbaas-mariadb/v1.1.2",
+		UserAgent:          "ionos-cloud-sdk-go-dbaas-mariadb/v1.1.3",
 		Debug:              false,
 		Username:           username,
 		Password:           password,
@@ -186,6 +186,14 @@ func NewConfiguration(username, password, token, hostUrl string) *Configuration 
 			},
 		},
 		OperationServers: map[string]ServerConfigurations{},
+	}
+	if hostUrl != "" {
+		cfg.Servers = ServerConfigurations{
+			{
+				URL:         getServerUrl(hostUrl),
+				Description: "overriden endpoint",
+			},
+		}
 	}
 	return cfg
 }
